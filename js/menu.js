9d4721ac4309f5e3ef34a2ebf1c8ba450f17ddc7
Germanilu <lucianogermani93@gmail.com>
2022-05-18 23:05:13 +0200

    Añadido js para mostrar cada menu

---
+++ js/menu.js
@@ -0,0 +1,28 @@
+
+const MostrarEntrantes = () =>{
+    document.getElementById("entrantes").style.display = "";
+    document.getElementById("primeros").style.display = "none";
+    document.getElementById("segundos").style.display = "none";
+    document.getElementById("postres").style.display = "none";
+}
+
+const MostrarPrimeros = () =>{
+    document.getElementById("entrantes").style.display = "none";
+    document.getElementById("primeros").style.display = "";
+    document.getElementById("segundos").style.display = "none";
+    document.getElementById("postres").style.display = "none";
+}
+
+const MostrarSegundos = () =>{
+    document.getElementById("entrantes").style.display = "none";
+    document.getElementById("primeros").style.display = "none";
+    document.getElementById("segundos").style.display = "";
+    document.getElementById("postres").style.display = "none";
+}
+
+const MostrarPostres = () =>{
+    document.getElementById("entrantes").style.display = "none";
+    document.getElementById("primeros").style.display = "none";
+    document.getElementById("segundos").style.display = "none";
+    document.getElementById("postres").style.display = "";
+}
