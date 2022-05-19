558891ba3a83b15b6a91c9dd4a5b6698b15a1a2c
Germanilu <lucianogermani93@gmail.com>
2022-05-19 19:48:48 +0200

    Update scrollReveal on menu

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -21,3 +21,26 @@ ScrollReveal().reveal('.reveal-d1500', {
     delay: 1500,
     duration: 3000    
 });
+
+// Scroll Reveal Carta
+
+ScrollReveal().reveal('.reveal-d500', {
+    delay: 500,
+    duration: 3000    
+});
+ScrollReveal().reveal('.reveal-d600', {
+    delay: 600,
+    duration: 3000    
+});
+ScrollReveal().reveal('.reveal-d700', {
+    delay: 700,
+    duration: 3000    
+});
+ScrollReveal().reveal('.reveal-d900', {
+    delay: 900,
+    duration: 3000    
+});
+ScrollReveal().reveal('.reveal-d1000', {
+    delay: 1000,
+    duration: 3000    
+});
