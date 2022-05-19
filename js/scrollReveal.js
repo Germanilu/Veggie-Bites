fc19778863e219c9cf09deac1a0116daa7d2147b
Germanilu <lucianogermani93@gmail.com>
2022-05-19 18:38:58 +0200

    Adding scroll-reveal to index.html

---
+++ js/scrollReveal.js
@@ -0,0 +1,18 @@
+// Scroll Reveal 
+ScrollReveal().reveal('.reveal-d500', {
+     delay: 500,
+     duration: 3000    
+});
+
+ScrollReveal().reveal('.reveal-d800', {
+     delay: 800,
+     duration: 3000  
+    
+});
+
+ScrollReveal().reveal('.reveal-d1300', {
+     delay: 1300, 
+     duration: 3000  
+
+});
+
