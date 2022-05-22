bf47aff0cddf3c58525438a89119c3af26f367f4
Germanilu <lucianogermani93@gmail.com>
2022-05-22 13:11:50 +0200

    corrección ortográfica

--- js/menu.js
+++ js/menu.js
@@ -25,7 +25,6 @@ const MostrarPostres = () =>{
     document.getElementById("primeros").style.display = "none";
     document.getElementById("segundos").style.display = "none";
     document.getElementById("postres").style.display = "";
-   
 }
 
 
